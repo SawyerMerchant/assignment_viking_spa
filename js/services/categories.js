85fa950263a989b11886671151dbc7a8e7b26c47
John Sawyer <sawyer@sawyermerchant.com>
2017-04-23 13:56:30 -0400

    Add categories

---
+++ js/services/categories.js
@@ -0,0 +1,29 @@
+VikingStore.factory('categoriesService', ['productsService', function(productServices){
+  var _categories = [];
+  var _id = 1;
+
+  var _makeCategories = function(){
+    var categories = [
+      {
+        "id": 1,
+        "name": "Bikes",
+        "productIDs": []
+      },
+      {
+        "id": 2,
+        "name": "Tires",
+        "productIDs": []
+      },
+      {
+        "id": 3,
+        "name": "Seats",
+        "productIDs": []
+      },
+      {
+        "id": 4,
+        "name": "Handlebars",
+        "productIDs": []
+      }
+    ];
+  };
+}]);
